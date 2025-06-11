2a459aedc21526a5a418bdcd510afe11fab5259f
sam2829 <samscarisbrick@outlook.com>
2025-06-11 15:44:00 +0100

    Add progress bar to show timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { Timer as TimerProps } from "../store/timers-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  useEffect(() => {
+    setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
